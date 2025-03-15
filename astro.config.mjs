4fc2d135080ff45028effedc6dbabafb9f988bd8
Mario-Coxe <mario.coxe@ideiasdinamicas.com>
2025-03-15 18:07:07 +0100

    fix: fixing base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+
 
 export default defineConfig({
-  site: "https://mario-coxe.github.io/mariocoxe.com/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  base: "mariocoxe.com",
+  output: 'static',
 });
