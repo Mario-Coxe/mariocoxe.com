a7c107fd646f1b3ca1c40c0cbe311e2e518d39ab
Mario-Coxe <mario.coxe@ideiasdinamicas.com>
2025-03-15 17:40:06 +0100

    refactory: removal of existing projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://mario-coxe.github.io/mariocoxe.com/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
